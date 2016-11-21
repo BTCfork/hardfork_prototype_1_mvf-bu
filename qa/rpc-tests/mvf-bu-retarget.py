02bac1287396da5fdf26e9af8719acf88f1b5948
marlengit <marlen@safe-mail.net>
2016-11-21 12:14:06 +0100

    Merge difficulty testing.

--- qa/rpc-tests/mvf-bu-retarget.py
+++ qa/rpc-tests/mvf-bu-retarget.py
@@ -42,7 +42,7 @@ class MVF_RETARGET_Test(BitcoinTestFramework):
             assert_equal(False, self.is_fork_triggered_on_node(n))
         print "Fork did not trigger prematurely"
 
-        # check that fork triggers for nodes 0 and 1 at designated height
+        # check that fork triggers for nodes at designated height
         # move all nodes to height 100
         for n in xrange(len(self.nodes)):
             self.nodes[n].generate(1)
@@ -52,12 +52,15 @@ class MVF_RETARGET_Test(BitcoinTestFramework):
 
         # use to track how many times the same bits are used in a row
         prev_block = 0
-        count_bits_used = 0
+        count_bits_used = float(-1)
         diffadjinterval = 0
-        prev_block_delta = 0
+        next_block_time = 0
+        prev_block_delta = float(0)
+        best_diff_expected = float(0)
+        prev_blocks_delta_avg = float(0)
 
         # start generating MVF blocks with varying time stamps
-        print "nBits changed @ Time,Block,Delta(secs),nBits,Used,Difficulty"
+        print "nBits changed @ Time,Block,Delta(secs),nBits,Used,Difficulty,Next Difficulty"
         for n in xrange(200 * 24 * 60 * 60 / 600): #26640
             best_block_hash = self.nodes[0].getbestblockhash()
             best_block = self.nodes[0].getblock(best_block_hash, True)
@@ -67,40 +70,67 @@ class MVF_RETARGET_Test(BitcoinTestFramework):
             if prev_block['bits'] == best_block['bits']:
                 count_bits_used += 1
             else:
+                prev_blocks_delta_avg = prev_block_delta / count_bits_used
+                diff_factor = float(600) / prev_blocks_delta_avg
+                best_diff_expected = prev_block['difficulty'] * diff_factor
 
-                print "%s,%d,%d,%s,%d,%s " %(
+
+                print "%s,%d,%d,%s,%d,%f,%f,%f " %(
                     time.strftime("%Y-%m-%d %H:%M",time.gmtime(prev_block['time'])),
                     prev_block['height'],
-                    prev_block_delta,
+                    prev_blocks_delta_avg,
                     prev_block['bits'],
                     count_bits_used,
-                    prev_block['difficulty'])
+                    prev_block['difficulty'],
+                    best_diff_expected,
+                    diff_factor)
 
                 if prev_block['bits'] <> "207fffff":
                     assert_less_than_equal(count_bits_used, diffadjinterval)
 
                 count_bits_used = 1
+                prev_block_delta = 0
             #### end if prev_block['bits'] == best_block['bits']
 
-            # print info for every block
-            #print "%s :: %s :: %f :: %s" %(
-                #best_block['height'],
-                #time.strftime("%H:%M",time.gmtime(best_block['time'])),
-                #best_block['difficulty'],
-                #best_block['bits'])
-
-            if n <= 36 :
-                # simulate slow blocks just after the fork i.e. low hash power/high difficulty
+            # determine block time interval
+            if n in range(0,11) :
+                next_block_time = next_block_time + 50
+            elif n in range(12,18) :
+                next_block_time = 300
+            elif n in range(19,26) :
+                next_block_time = 1200
+            elif n in range(27,500) :
+                next_block_time = 600
+            elif n in range(501,2500) :
+                 # simulate slow blocks just after the fork i.e. low hash power/high difficulty
                 # this will cause bits to hit the limit 207fffff
                 next_block_time = randint(4000,6000)
             else:
                 # simulate ontime blocks i.e. hash power/difficult around 600 secs
                 next_block_time = randint(500,700)
 
+
             self.nodes[0].setmocktime(best_block['time'] + next_block_time)
 
-            prev_block_delta = best_block['time'] - prev_block['time']
+            prev_block_delta = prev_block_delta + (best_block['time'] - prev_block['time'])
             diffadjinterval = self.nodes[0].getblockchaininfo()['difficultyadjinterval']
+
+            # print info for every block
+            #print "%s :: %s :: %d :: %f :: %f" %(
+                #best_block['height'],
+                #time.strftime("%H:%M",time.gmtime(best_block['time'])),
+                #prev_block_delta,
+                #best_block['difficulty'],
+                #best_diff_expected)
+
+            #print "%d,%d,%f,%f,%f,%f" % (
+                #n, next_block_time,
+                #prev_block_delta / count_bits_used,
+                #prev_block_delta / count_bits_used / 600,
+                #best_diff_expected,
+                #best_block['difficulty'])
+
+            #assert_equal(best_block['difficulty'],prev_block['difficulty'] + (prev_block['difficulty'] * (prev_block_delta/600)))
 
             self.nodes[0].generate(1)
 
